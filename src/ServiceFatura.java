0f1ca647ce8beab23e263c5b4a24382c5db2ac4e
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-07 19:57:39 -0300

    Criar metodo cadastrarFatura()

--- src/ServiceFatura.java
+++ src/ServiceFatura.java
@@ -1,2 +1,12 @@
-public class SistemaFatura {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceFatura {
+    private static List<Fatura> listaFaturas = new ArrayList<>();
+
+    public static Fatura cadastrarFatura(Cliente cliente, double valor, String dataVencimento) {
+        Fatura fatura = new Fatura(cliente, valor, dataVencimento);
+        listaFaturas.add(fatura);
+        return fatura;
+    }
 }
