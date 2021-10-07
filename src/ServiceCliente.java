875b8d89bbfc1d85a3ed8b4af13af808aaa67b7b
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-07 19:58:26 -0300

    Criar metodo verificarEmail

--- src/ServiceCliente.java
+++ src/ServiceCliente.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class SistemaCliente {
-    public static Cliente cadastrarCliente(){
-        
+public class ServiceCliente {
+    private static List<Cliente> listaClientes = new ArrayList<>();
+
+    public static void verificarEmail(String email) throws Exception{
+        if (!email.contains("@")) {
+           throw new Exception("O Email digitado não é válido. Tente novamente!");
+        } else {
+            System.out.println("___Cliente cadastrado com sucesso!___");
+        }
+    }
+
+    public static Cliente cadastrarCliente(String nome, String email) {
+        Cliente cliente = new Cliente(nome, email);
+        listaClientes.add(cliente);
+        return cliente;
     }
 }
