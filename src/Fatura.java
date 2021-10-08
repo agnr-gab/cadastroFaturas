7234a89f94b0b2d5a80b80cc071f708d5aba79e5
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-08 08:43:06 -0300

    Criar get list

--- src/Fatura.java
+++ src/Fatura.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fatura {
     private Cliente cliente;
     private double valor;
     private String dataVencimento;
+
+    private static List<Fatura> listaFaturas = new ArrayList<>();
+
+    public static List<Fatura> getListaFaturas() {
+        return listaFaturas;
+    }
 
     public Fatura() {
     }
@@ -42,6 +51,7 @@ public class Fatura {
         retorno.append("Cliente\n" + cliente);
         retorno.append("\tValor: R$" + valor);
         retorno.append("\tData de Vencimento: " + dataVencimento);
+        retorno.append("\tQuantidade de faturas: " + );
         return retorno.toString();
     }
 }
