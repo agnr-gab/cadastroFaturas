8ae4f6d19a7d6040076e6acac38737073634eda8
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-08 09:12:06 -0300

    Setar execução na main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        try {
 
-        } catch (Exception erro) {
-            System.out.println(erro.getMessage());
+    public static void main(String[] args) throws Exception {
+
+        boolean loop = true;
+
+        while (loop) {
+
+            try {
+                loop = Sistema.executar();
+
+            } catch (Exception erro) {
+                System.out.println(erro.getMessage());
+
+            }
         }
     }
 }
