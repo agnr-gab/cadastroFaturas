7005ad787a92e9d68465e2c2f20746caca5e3e67
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-07 22:30:09 -0300

    Adicionar metodo verificarFaturas

--- src/ServiceFatura.java
+++ src/ServiceFatura.java
@@ -5,9 +5,17 @@ public class ServiceFatura {
     private static List<Fatura> listaFaturas = new ArrayList<>();
 
     public static Fatura cadastrarFatura(String email, double valor, String dataVencimento) throws Exception {
-        Cliente cliente = ServiceCliente.verificarClienteCadastrado(email);
+        Cliente cliente = ServiceCliente.verificarEmailExistente(email);
         Fatura fatura = new Fatura(cliente,valor, dataVencimento);
         listaFaturas.add(fatura);
         return fatura;
+    }
+    public static List<Fatura> verificarFaturas(String email) {
+        List<Fatura> listaFaturaClientes = new ArrayList<>();
+        for (Fatura referenciaFatura: listaFaturas) {
+            if (referenciaFatura.getCliente().getEmail().equals(email)){
+                listaFaturaClientes.add(referenciaFatura);
+            }
+        } return listaFaturaClientes;
     }
 }
