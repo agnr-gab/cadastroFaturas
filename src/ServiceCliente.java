427c5af27bc7ed83e10f72905ee4e96ecbb54232
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-07 22:21:17 -0300

    Alterar ordem dos metodos

--- src/ServiceCliente.java
+++ src/ServiceCliente.java
@@ -4,22 +4,23 @@ import java.util.List;
 public class ServiceCliente {
     private static List<Cliente> listaClientes = new ArrayList<>();
 
-    public static Cliente verificarClienteCadastrado(String email) throws Exception {
-        for (Cliente referenciaCliente : listaClientes) {
-            if (referenciaCliente.getEmail().equals(email)) {
-                return referenciaCliente;
-            }
-
-        }
-        throw new Exception("Cliente não cadastrado no banco de dados! Verifique novamente!");
-    }
-
-    public static void verificarEmail(String email) throws Exception {
+    public static void verificarEmailInvalido(String email) throws Exception {
         if (!email.contains("@")) {
+            //não aceita email sem @
             throw new Exception("O Email digitado não é válido. Tente novamente!");
         } else {
             System.out.println("___Cliente cadastrado com sucesso!___");
         }
+    }
+
+    public static Cliente verificarEmailExistente(String email) throws Exception {
+        for (Cliente referenciaCliente : listaClientes) {
+            //Percorre a lista de clientes e verifica se tem email no banco de dados
+            if (referenciaCliente.getEmail().equals(email)) {
+                return referenciaCliente;
+            }
+        }
+        throw new Exception("Cliente não cadastrado no banco de dados! Verifique novamente!");
     }
 
     public static Cliente cadastrarCliente(String nome, String email) {
